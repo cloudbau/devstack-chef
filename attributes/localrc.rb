57246e58e9e4a7552b90f4595db5b86e709cf0fe
Stephan Renatus <s.renatus@cloudbau.de>
2013-10-24 08:14:22 +0200

    make attribute changes backward-compatible

--- attributes/localrc.rb
+++ attributes/localrc.rb
@@ -1,64 +1,9 @@
 default['devstack']['localrc'] = {
-  host_ip:               '127.0.0.1',
-  admin_password:        'password',
-  database_password:     'password',
-  rabbit_password:       'password',
-  service_token:         'password',
-  service_password:      'password',
-  dest:                  '/opt/stack',
-  # reclone:               'yes',
-  keystone_token_format: 'PKI',
-  swift_replicas:        1,
-  swift_hash:            '011688b44136573e209e',
-  logfile:               '/opt/stack/logs/stack.sh.log',
-  verbose:               true,
-  log_color:             true,
-  screen_logdir:         '/opt/stack/logs',
-  enabled_services:  [
-    'rabbit', # 'zeromq', 'qpid'
-    'mysql',
-    'key',               # Keystone
-    'horizon',           # Horizon
-    'n-api',             # Nova
-    'n-crt',
-    'n-obj',
-    'n-cpu',
-    'n-cond',
-    'n-sch',
-    'g-api',             # Glance
-    'g-reg',
-    # 's-proxy',         # Swift
-    # 's-obj',
-    # 's-container',
-    # 's-account',
-    'q-svc',             # Neutron
-    'q-agt',
-    'q-dhcp',
-    'q-l3',
-    'q-meta',
-    'neutron',
-    'q-lbaas',           # Neutron extensions
-    'q-fwaas',
-    # 'q-vpn',           # this effectively disables q-l3
-    'cinder',            # Cinder
-    'c-api',
-    'c-vol',
-    'c-sch',
-    'heat',              # Heat
-    'h-api',
-    'h-api-cfn',
-    'h-api-cw',
-    'h-eng',
-    'ceilometer-acompute',
-    'ceilometer-acentral',
-    'ceilometer-collector',
-    'ceilometer-api',
-    'ceilometer-alarm-notify',
-    'ceilometer-alarm-eval'
-    ],
-  horizon_repo: 'https://github.com/openstack/horizon',
-  horizon_branch:        'master',
-  q_plugin:              'ml2',
-  enable_tenant_tunnels: true,
-  apache_enabled_services: 'keystone' # [ 'keystone', 'swift']
+  host_ip:               node['devstack']['host-ip'],
+  admin_password:        node['devstack']['admin-password'],
+  database_password:     node['devstack']['database-password'],
+  rabbit_password:       node['devstack']['rabbit-password'],
+  service_token:         node['devstack']['service-token'],
+  service_password:      node['devstack']['service-password'],
+  dest:                  node['devstack']['dest']
 }
