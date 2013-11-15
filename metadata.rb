74e344ef1524def0e16f5f12b4ec1a908732758e
Edmund Haselwanter <edmund@haselwanter.com>
2013-11-15 15:07:49 +0100

    point to new README.md, using markdown. berkshelf explodes without this

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'devstack'
 maintainer       "Rackspace US, Inc"
 license          "Apache 2.0"
 description      'Installs/Configures devstack'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.org'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
 %w{ debian ubuntu }.each do |os|
